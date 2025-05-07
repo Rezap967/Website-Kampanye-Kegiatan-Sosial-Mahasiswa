2144b7cd7a32ca44dd9fac0c6a026e4d70f377b2
Rezap967 <149461517+Rezap967@users.noreply.github.com>
2025-05-07 18:49:16 +0700

    first commit

---
+++ js/custom.js
@@ -0,0 +1,51 @@
+
+  (function ($) {
+  
+  "use strict";
+
+    // MENU
+    $('.navbar-collapse a').on('click',function(){
+      $(".navbar-collapse").collapse('hide');
+    });
+    
+    // CUSTOM LINK
+    $('.smoothscroll').click(function(){
+      var el = $(this).attr('href');
+      var elWrapped = $(el);
+      var header_height = $('.navbar').height();
+  
+      scrollToDiv(elWrapped,header_height);
+      return false;
+  
+      function scrollToDiv(element,navheight){
+        var offset = element.offset();
+        var offsetTop = offset.top;
+        var totalScroll = offsetTop-navheight;
+  
+        $('body,html').animate({
+        scrollTop: totalScroll
+        }, 300);
+      }
+    });
+  
+  })(window.jQuery);
+
+  // Smooth scrolling for anchor links
+function initSmoothScroll() {
+  document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+      anchor.addEventListener('click', function(e) {
+          e.preventDefault();
+          const target = document.querySelector(this.getAttribute('href'));
+          if (target) {
+              target.scrollIntoView({
+                  behavior: 'smooth'
+              });
+          }
+      });
+  });
+}
+
+// Panggil fungsi saat dokumen siap
+document.addEventListener('DOMContentLoaded', initSmoothScroll);
+
+
